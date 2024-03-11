49ecd450d001c107ff973a66316b11f4188ad225
Denner Moraes <denner.moraes@anestech.com.br>
2024-03-11 12:43:42 +0000

    Final commit

--- src/App.js
+++ src/App.js
@@ -1,72 +1,9 @@
 import React from 'react';
-import { useApiOperations } from './services/apiFunction';
+import CardPage from './pages/CardsPage';
 
 function App() {
-  const {
-    newData,
-    setNewData,
-    allData,
-    editingIndex,
-    editedData,
-    setEditedData,
-    handleInsertData,
-    handleStartEditing,
-    handleSaveEditing,
-    handleCancelEditing,
-    handleDelete,
-  } = useApiOperations();
-
-  const handleChange = (e) => {
-    // Certifique-se de que e.target.name corresponde aos nomes dos campos em newData
-    setNewData({ ...newData, [e.target.name]: e.target.value });
-  };
-
   return (
-    <div>
-      <h1>Seu Componente</h1>
-      <div>
-        <h2>Inserir Dados:</h2>
-        <label>Nome:
-          <input type="text" name="name" value={newData.name} onChange={handleChange} />
-        </label>
-        <label>Descrição:
-          <input type="text" name="description" value={newData.description} onChange={handleChange} />
-        </label>
-        <button onClick={handleInsertData}>Inserir Dados</button>
-      </div>
-
-      <div>
-        <h2>Dados Atuais:</h2>
-        <ul>
-          {allData.map((item, index) => (
-            <li key={index}>
-              {item.name} - {item.description}
-              {editingIndex === index ? (
-                <div>
-                  <input
-                    type="text"
-                    value={editedData.name || ''}
-                    onChange={(e) => setEditedData({ ...editedData, name: e.target.value })}
-                  />
-                  <input
-                    type="text"
-                    value={editedData.description || ''}
-                    onChange={(e) => setEditedData({ ...editedData, description: e.target.value })}
-                  />
-                  <button onClick={handleSaveEditing}>Salvar</button>
-                  <button onClick={handleCancelEditing}>Cancelar</button>
-                </div>
-              ) : (
-                <div>
-                  <button onClick={() => handleStartEditing(index, item)}>Editar</button>
-                  <button onClick={() => handleDelete(item.id)}>Excluir</button>
-                </div>
-              )}
-            </li>
-          ))}
-        </ul>
-      </div>
-    </div>
+    <CardPage></CardPage>
   );
 }
 
--- src/services/apiFunction.js
+++ src/services/apiFunction.js
@@ -1,20 +1,21 @@
-// apiFunctions.js
 import { useState, useEffect } from 'react';
 import { fetchData, insertData, updateData, deleteData } from './api';
 
 export function useApiOperations() {
+  // State variables for managing new data, all data, editing index, and edited data
   const [newData, setNewData] = useState({});
   const [allData, setAllData] = useState([]);
   const [editingIndex, setEditingIndex] = useState(null);
   const [editedData, setEditedData] = useState({});
 
+  // Fetch data from the API on component mount
   useEffect(() => {
     const fetchDataFromApi = async () => {
       try {
         const data = await fetchData();
         setAllData(data);
       } catch (error) {
-        console.error('Erro ao carregar dados:', error);
+        console.error('Error loading data:', error);
       }
     };
 
@@ -22,46 +23,65 @@ export function useApiOperations() {
   }, []);
 
   const handleInsertData = async () => {
-    try {
-      const insertedData = await insertData(newData);
-      setAllData(prevData => [...prevData, insertedData]);
-    } catch (error) {
-      console.error('Erro ao inserir dados:', error);
+    if (!newData.title || !newData.date || !newData.local) {
+      alert('Fill in the mandatory fields (title, date, location)');
+      return;
+    } else {
+      try {
+        const insertedData = await insertData(newData);
+        // Update state with the newly inserted data
+        setAllData(prevData => [...prevData, insertedData]);
+        alert('Created successfully');
+      } catch (error) {
+        console.error('Error inserting data:', error);
+      }
     }
   };
 
+  //Sets the index and data for starting the editing process.
   const handleStartEditing = (index, data) => {
     setEditingIndex(index);
     setEditedData(data);
   };
+
+  //Handles saving the edited data to the API.
 
   const handleSaveEditing = async () => {
     try {
       const updatedData = await updateData(editingIndex, editedData);
       const updatedAllData = [...allData];
       updatedAllData[editingIndex] = updatedData;
+      // Update state with the edited data and reset editing state
       setAllData(updatedAllData);
       setEditingIndex(null);
       setEditedData({});
     } catch (error) {
-      console.error('Erro ao salvar edições:', error);
+      console.error('Error saving edits:', error);
     }
   };
+
+  //Cancels the editing process and resets editing state.
 
   const handleCancelEditing = () => {
     setEditingIndex(null);
     setEditedData({});
   };
 
+
+  //Handles the deletion of data from the API.
+
   const handleDelete = async (id) => {
     try {
       await deleteData(id);
+      // Update state by removing the deleted data
       setAllData(prevData => prevData.filter(item => item.id !== id));
+      alert('Deleted card')
     } catch (error) {
-      console.error('Erro ao excluir dados:', error);
+      console.error('Error deleting data:', error);
     }
   };
 
+  // Return the state variables and functions for API operations
   return {
     newData,
     allData,
